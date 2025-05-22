4337bebaaeb5e0305c144902836773d4e4125fcb
RyanClayKao <f256987584@gmail.com>
2025-05-22 15:49:38 +0800

    了解到 windown.orientation 已經被淘汰，可以改用 window.screen.orientation.type 來判斷裝置直橫，以避免手機板出現打字區域時誤判為橫式的問題

--- 14-Orientation/js/14-3.js
+++ 14-Orientation/js/14-3.js
@@ -1,12 +1,27 @@
 $(function(){
 	window.addEventListener("orientationchange",onOrientationchange ,false);
 	function onOrientationchange() {
-		if (window.orientation === 180 || window.orientation === 0) {
-			/*orientation: portrait 直式*/
+		// if (window.orientation === 180 || window.orientation === 0) {
+		// 	/*orientation: portrait 直式*/
+		// 	$("#orientation").hide();
+		// }
+		// if (window.orientation === 90 || window.orientation === -90 ){
+		// 	/*orientation: landscape 橫式*/
+		// 	$("#orientation").show();
+		// }
+
+		// MEMO: window.orientation 已經被淘汰使用，取而代之的是 window.screen.orientation.type，一樣可以縮減不寫 window
+		// 直向
+		if (screen.orientation.type === "portrait-primary" ||
+				screen.orientation.type === "portrait-secondary"
+		) {
 			$("#orientation").hide();
 		}
-		if (window.orientation === 90 || window.orientation === -90 ){
-			/*orientation: landscape 橫式*/
+
+		// 橫向
+		if (screen.orientation.type === "landscape-primary" ||
+				screen.orientation.type === "landscape-secondary"
+		) {
 			$("#orientation").show();
 		}
 	}
